b1a5fbd83602bc837cb83a4fb38492220e766a77
YunaH <Yunahan53@gmail.com>
2026-02-07 15:25:52 -0500

    Added functionality to generate random password upon clicking Generate Password button.

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from tkinter import *
 from tkinter import messagebox
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+import random
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_list = [random.choice(letters) for _ in range(random.randint(8, 10))]
+    password_list += [random.choice(symbols) for _ in range(random.randint(2, 4))]
+    password_list += [random.choice(numbers) for _ in range(random.randint(2, 4))]
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+
+    password_entry.delete(0, END)
+    password_entry.insert(0, password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
@@ -56,7 +72,7 @@ password_entry = Entry(width=41)
 password_entry.grid(column=1, row=3)
 
 # Display the buttons
-password_button = Button(text="Generate Password")
+password_button = Button(text="Generate Password", command=generate_password)
 password_button.grid(column=2, row=3)
 
 add_button =Button(text="Add", width=50, command=save)
